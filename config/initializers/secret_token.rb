abe35780f910d3a96ef271f57d7f59d1d766e92d
Craig Miller <cmiller@cs.depaul.edu>
2013-01-21 13:41:37 -0600

    Initial skeleton program

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cinema::Application.config.secret_token = '08ef82078092ae0c20caae4b0d3c0db07c7638b98f3a24d6eb18e8cd46d128d4551995bc178e8feb865c313a46cf0cb2cef3cdb39d8def68a66d0e62fdd686b8'
