dc575bf53a3eadc6a2fd47c3335d93f18f861b3e
Craig Miller <cmiller@cs.depaul.edu>
2013-01-28 13:41:11 -0600

    Added validations to the movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,14 @@
 class Movie < ActiveRecord::Base
   attr_accessible :seen, :title, :year
+  
+  validates :title, :presence => true
+  validates :year, :numericality => true
+  validate :check_year_for_range
+  
+  def check_year_for_range
+    if self.year < 1900 then
+      self.errors.add(:year, "must be 1900 or more recent")
+    elsif self.year > 2013 then
+      self.errors.add(:year, "must be 2013 or older")
+    end
 end
