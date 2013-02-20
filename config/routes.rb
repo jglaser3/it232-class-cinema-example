20ee1709d6ecbae2c3b821f904349b4db5040649
Craig Miller <cmiller@cs.depaul.edu>
2013-02-20 14:56:54 -0600

    Added rest of basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -70,7 +70,7 @@ Cinema::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'movies#index'
 
   # See how all your routes lay out with "rake routes"
 
